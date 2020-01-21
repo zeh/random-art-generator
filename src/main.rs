f3f105b45310d03b838308decaf06ddd9c2671c1
Zeh Fernando <zeh@work.co>
2020-01-21 14:08:07 -0500

    Command line: added parameter parsing via structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// Progressively generate an image based on a target
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// The target image
+    #[structopt(short, long, parse(from_os_str))]
+    target: PathBuf,
+
+    /// The output image filename
+    #[structopt(short, long, parse(from_os_str))]
+    output: PathBuf,
+
+    /// The input image filename, if any
+    #[structopt(short, long, default_value = "", parse(from_os_str))]
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Opt::from_args();
+    println!("Done.");
 }
