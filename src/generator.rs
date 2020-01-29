61f09226f3a3f822361f6c6b3f1088fea93c4a18
Zeh Fernando <zeh@work.co>
2020-01-29 09:29:09 -0500

    Generator: speed up generation from ~29ms to ~26ms on a release build
    Instead of creating a new bitmap using a function generator, we make a clone of the existing canvas and then simply paint a rectangle.

--- src/generator.rs
+++ src/generator.rs
@@ -116,25 +116,28 @@ impl Painter for RectPainter {
 		let h = canvas.dimensions().1 as f32;
 		let random_w = random::<f32>();
 		let random_h = random::<f32>();
-		let rect_w = (random_w * random_w * random_w * w).round() as u32;
-		let rect_h = (random_h * random_h * random_h * h).round() as u32;
-		let rect_x = (random::<f32>() * (w - rect_w as f32)).round() as u32;
-		let rect_y = (random::<f32>() * (h - rect_h as f32)).round() as u32;
-		// let pixel = image::RGB(255, 255, 255);
-		// canvas.put_pixel(rect_x, rect_y, p);
-		// return canvas;
+		let rect_w = random_w * random_w * random_w * w;
+		let rect_h = random_h * random_h * random_h * h;
+		let rect_x = random::<f32>() * (w - rect_w);
+		let rect_y = random::<f32>() * (h - rect_h);
+
+		let x1 = rect_x as u32;
+		let x2 = (rect_x + rect_w) as u32 + 1;
+		let y1 = rect_y as u32;
+		let y2 = (rect_y + rect_h) as u32 + 1;
 
 		let r = random::<u8>();
 		let g = random::<u8>();
 		let b = random::<u8>();
 
-		let painted_canvas = RgbImage::from_fn(w.round() as u32, h.round() as u32, |x, y| {
-			if x >= rect_x && x <= rect_x + rect_w && y >= rect_y && y <= rect_y + rect_h {
-				image::Rgb([r, g, b])
-			} else {
-				canvas[(x, y)]
+		let pixel = image::Rgb([r, g, b]);
+
+		let mut painted_canvas = canvas.clone();
+		for x in x1..x2 {
+			for y in y1..y2 {
+				painted_canvas.put_pixel(x, y, pixel);
 			}
-		});
+		}
 
 		return painted_canvas
 	}
