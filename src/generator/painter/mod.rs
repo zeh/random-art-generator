c4be4e771b0a726c960df46b231d666e39c584dd
Zeh Fernando <zeh@work.co>
2020-02-04 11:46:27 -0500

    PainterRect: parameterized and reorganized some rect filling options

--- src/generator/painter/mod.rs
+++ src/generator/painter/mod.rs
@@ -1,41 +1,71 @@
 use image::{RgbImage};
-use rand::{random};
+use rand::{Rng,thread_rng};
 
 pub trait Painter {
 	fn new() -> Self;
 	fn paint(&self, canvas: &RgbImage) -> RgbImage;
 }
 
+struct Options {
+	min_width: f64,
+	max_width: f64,
+	min_height: f64,
+	max_height: f64,
+	width_distribution: f64,
+	height_distribution: f64,
+}
+
 pub struct RectPainter {
+	options: Options,
 }
 
 impl Painter for RectPainter {
 	fn new() -> RectPainter {
-		RectPainter { }
+		let options = Options {
+			min_width: 0.0,
+			max_width: 1.0,
+			min_height: 0.0,
+			max_height: 1.0,
+			width_distribution: 3.0, // Cubic
+			height_distribution: 3.0, // Cubic
+		};
+
+		RectPainter {
+			options,
+		}
 	}
 
 	fn paint(&self, canvas: &RgbImage) -> RgbImage {
-		//let rng = rand::thread_rng().gen_range(1, 101);
-		let w = canvas.dimensions().0 as f32;
-		let h = canvas.dimensions().1 as f32;
-		let random_w = random::<f32>();
-		let random_h = random::<f32>();
-		let rect_w = random_w * random_w * random_w * w;
-		let rect_h = random_h * random_h * random_h * h;
-		let rect_x = random::<f32>() * (w - rect_w);
-		let rect_y = random::<f32>() * (h - rect_h);
+		let mut rng = thread_rng();
 
-		let x1 = rect_x as u32;
-		let x2 = (rect_x + rect_w) as u32;
-		let y1 = rect_y as u32;
-		let y2 = (rect_y + rect_h) as u32;
+		let image_w = canvas.dimensions().0 as f64;
+		let image_h = canvas.dimensions().1 as f64;
 
-		let r = random::<u8>();
-		let g = random::<u8>();
-		let b = random::<u8>();
+		// Find dimensions in the 0-1 range
+		let random_w: f64 = rng.gen_range(0.0f64, 1.0f64).powf(self.options.width_distribution);
+		let random_h: f64 = rng.gen_range(0.0f64, 1.0f64).powf(self.options.height_distribution);
 
+		// Lerp dimensions into pixels
+		let rect_w: f64 = (self.options.min_width + random_w * (self.options.max_width - self.options.min_width)) * image_w;
+		let rect_h: f64 = (self.options.min_height + random_h * (self.options.max_height - self.options.min_height)) * image_h;
+
+		// Distribute along the axis too
+		let rect_x: f64 = rng.gen_range(0.0f64, 1.0f64) * (image_w - rect_w);
+		let rect_y: f64 = rng.gen_range(0.0f64, 1.0f64) * (image_h - rect_h);
+
+		// Found final, round positions
+		let x1 = rect_x.round().max(0.0).min(image_w) as u32;
+		let x2 = (rect_x + rect_w).round().max(0.0).min(image_w) as u32;
+		let y1 = rect_y.round().max(0.0).min(image_w) as u32;
+		let y2 = (rect_y + rect_h).round().max(0.0).min(image_h) as u32;
+
+		// Determine color
+		let r = rng.gen_range(0u8, 255u8);
+		let g = rng.gen_range(0u8, 255u8);
+		let b = rng.gen_range(0u8, 255u8);
 		let pixel = image::Rgb([r, g, b]);
 
+		// Finally, paint
 		let mut painted_canvas = canvas.clone();
 		for x in x1..x2 {
 			for y in y1..y2 {
