4922a2ca9c2ccb4ea8a9f2011c1edfd870182e7e
Zeh Fernando <zeh@work.co>
2020-02-04 11:47:01 -0500

    PainterRect: painting can be blended using alpha

--- src/generator/painter/mod.rs
+++ src/generator/painter/mod.rs
@@ -1,5 +1,12 @@
-use image::{RgbImage};
-use rand::{Rng,thread_rng};
+use image::{Pixel, RgbImage};
+use rand::{Rng, rngs, thread_rng};
+
+fn get_random(rng: &mut rngs::ThreadRng, min: f64, max: f64) -> f64 {
+	if min == max {
+		return min;
+	};
+	rng.gen_range(min, max)
+}
 
 pub trait Painter {
 	fn new() -> Self;
@@ -11,6 +18,8 @@ struct Options {
 	max_width: f64,
 	min_height: f64,
 	max_height: f64,
+	min_alpha: f64,
+	max_alpha: f64,
 	width_distribution: f64,
 	height_distribution: f64,
 }
@@ -26,6 +35,8 @@ impl Painter for RectPainter {
 			max_width: 1.0,
 			min_height: 0.0,
 			max_height: 1.0,
+			min_alpha: 1.0,
+			max_alpha: 1.0,
 			width_distribution: 3.0, // Cubic
 			height_distribution: 3.0, // Cubic
 		};
@@ -42,16 +53,16 @@ impl Painter for RectPainter {
 		let image_h = canvas.dimensions().1 as f64;
 
 		// Find dimensions in the 0-1 range
-		let random_w: f64 = rng.gen_range(0.0f64, 1.0f64).powf(self.options.width_distribution);
-		let random_h: f64 = rng.gen_range(0.0f64, 1.0f64).powf(self.options.height_distribution);
+		let random_w: f64 = get_random(&mut rng, 0.0f64, 1.0f64).powf(self.options.width_distribution);
+		let random_h: f64 = get_random(&mut rng, 0.0f64, 1.0f64).powf(self.options.height_distribution);
 
 		// Lerp dimensions into pixels
 		let rect_w: f64 = (self.options.min_width + random_w * (self.options.max_width - self.options.min_width)) * image_w;
 		let rect_h: f64 = (self.options.min_height + random_h * (self.options.max_height - self.options.min_height)) * image_h;
 
 		// Distribute along the axis too
-		let rect_x: f64 = rng.gen_range(0.0f64, 1.0f64) * (image_w - rect_w);
-		let rect_y: f64 = rng.gen_range(0.0f64, 1.0f64) * (image_h - rect_h);
+		let rect_x: f64 = get_random(&mut rng, 0.0f64, 1.0f64) * (image_w - rect_w);
+		let rect_y: f64 = get_random(&mut rng, 0.0f64, 1.0f64) * (image_h - rect_h);
 
 		// Found final, round positions
 		let x1 = rect_x.round().max(0.0).min(image_w) as u32;
@@ -64,12 +75,26 @@ impl Painter for RectPainter {
 		let g = rng.gen_range(0u8, 255u8);
 		let b = rng.gen_range(0u8, 255u8);
 		let pixel = image::Rgb([r, g, b]);
+		let alpha: f64 = get_random(&mut rng, self.options.min_alpha, self.options.max_alpha);
+		let alpha_n: f64 = 1.0 - alpha;
 
 		// Finally, paint
 		let mut painted_canvas = canvas.clone();
 		for x in x1..x2 {
 			for y in y1..y2 {
-				painted_canvas.put_pixel(x, y, pixel);
+				let new_pixel;
+				if alpha == 1.0f64 {
+					// No need to blend
+					new_pixel = pixel;
+				} else {
+					// Blend pixels
+					let old_pixel = painted_canvas.get_pixel(x, y).channels();
+					let nr: u8 = (r as f64 * alpha + old_pixel[0] as f64 * alpha_n).round() as u8;
+					let ng: u8 = (g as f64 * alpha + old_pixel[1] as f64 * alpha_n).round() as u8;
+					let nb: u8 = (b as f64 * alpha + old_pixel[2] as f64 * alpha_n).round() as u8;
+					new_pixel = image::Rgb([nr, ng, nb]);
+				}
+				painted_canvas.put_pixel(x, y, new_pixel);
 			}
 		}
 
