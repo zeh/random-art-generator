b4975398f4fb9a3cd93fe31a355281594ab082ef
Zeh Fernando <zeh@work.co>
2020-02-08 11:02:44 -0500

    Utils: added a new pixel blending function

--- src/generator/utils/mod.rs
+++ src/generator/utils/mod.rs
@@ -7,3 +7,20 @@ pub fn get_random_range(rng: &mut rngs::ThreadRng, min: f64, max: f64) -> f64 {
 	};
 	rng.gen_range(min, max)
 }
+
+#[inline(always)]
+pub fn blend_pixel(bottom: &[u8], top: &[u8], alpha: f64) -> [u8; 3] {
+	// Return early if no need to blend
+	if alpha == 1.0f64 {
+		return [top[0], top[1], top[2]];
+	} else if alpha == 0.0f64 {
+		return [bottom[0], bottom[1], bottom[2]];
+	}
+
+	// Blend pixels
+	let alpha_n: f64 = 1.0f64 - alpha;
+	let nr: u8 = (top[0] as f64 * alpha + bottom[0] as f64 * alpha_n).round() as u8;
+	let ng: u8 = (top[1] as f64 * alpha + bottom[1] as f64 * alpha_n).round() as u8;
+	let nb: u8 = (top[2] as f64 * alpha + bottom[2] as f64 * alpha_n).round() as u8;
+	[nr, ng, nb]
+}
