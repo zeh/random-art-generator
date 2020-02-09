41ec8894a438cdcc6ee55dbd9db4215f3197f943
Zeh Fernando <zeh@work.co>
2020-02-09 13:54:04 -0500

    Utils: moved image diff and color matrix functions to utility modules

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -1,13 +1,11 @@
-use image::{DynamicImage, GenericImageView, Pixel, Rgb, RgbImage};
+use image::{DynamicImage, GenericImageView, Rgb, RgbImage};
 use painter::{Painter};
 use std::time::{Instant};
 
+use crate::generator::utils::image::{color_transform, diff};
+
 pub mod painter;
 pub mod utils;
-
-const LUMA_R: f64 = 0.2126;
-const LUMA_G: f64 = 0.7152;
-const LUMA_B: f64 = 0.0722;
 
 type Callback = fn(generator: &Generator, success: bool);
 
@@ -31,7 +29,7 @@ impl Generator {
 		let target = target_image.to_rgb();
 		let current = RgbImage::new(target_image.dimensions().0, target_image.dimensions().1);
 		Generator {
-			target: Generator::color_transform(&target, matrix),
+			target: color_transform(&target, matrix),
 			current: current,
 		}
 	}
@@ -48,7 +46,7 @@ impl Generator {
 	pub fn process(&mut self, attempts: u32, generations: u32, painter: impl Painter, cb: Option<Callback>) {
 		let mut new_candidate;
 		let mut new_diff;
-		let mut curr_diff = Generator::diff(&self.current, &self.target);
+		let mut curr_diff = diff(&self.current, &self.target);
 
 		println!("Starting attempts; initial difference from target is {:.2}%.", curr_diff * 100.0);
 
@@ -74,7 +72,7 @@ impl Generator {
 			time_elapsed_paint += time_started_paint.elapsed().as_micros();
 
 			time_started_diff = Instant::now();
-			new_diff = Generator::diff(&new_candidate, &self.target);
+			new_diff = diff(&new_candidate, &self.target);
 			time_elapsed_diff += time_started_diff.elapsed().as_micros();
 
 			if new_diff < curr_diff {
@@ -122,7 +120,7 @@ impl Generator {
 		let time_elapsed = time_started.elapsed().as_secs_f32();
 		let atts = total_att as f64 * 1000.0;
 
-		let final_diff = Generator::diff(&self.current, &self.target);
+		let final_diff = diff(&self.current, &self.target);
 		println!("Finished {} attempts in {:.3}s ({:.3}ms avg per attempt).", total_att, time_elapsed, time_elapsed_attempt as f64 / atts);
 		println!("Attempt took an average of {:.3}ms for painting, and {:.3}ms for diffing.", time_elapsed_paint as f64 / atts, time_elapsed_diff as f64 / atts);
 		println!("Produced {} generations, a {:.2}% success rate.", total_gen, total_gen as f64 / total_att as f64 * 100.0);
@@ -131,48 +129,5 @@ impl Generator {
 
 	pub fn get_current(&self) -> RgbImage {
 		return self.current.clone();
-	}
-
-	pub fn diff(a: &RgbImage, b: &RgbImage) -> f64 {
-		let w = a.dimensions().0;
-		let h = a.dimensions().1;
-		let num_pixels = w * h;
-
-		let mut diff_sum_r: i32 = 0;
-		let mut diff_sum_g: i32 = 0;
-		let mut diff_sum_b: i32 = 0;
-
-		let samples_a = a.as_flat_samples().samples;
-		let samples_b = b.as_flat_samples().samples;
-
-		let skip_step = 1;
-
-		for (p_a, p_b) in samples_a.chunks_exact(3).zip(samples_b.chunks_exact(3)).step_by(skip_step) {
-			diff_sum_r += (p_a[0] as i32 - p_b[0] as i32).abs();
-			diff_sum_g += (p_a[1] as i32 - p_b[1] as i32).abs();
-			diff_sum_b += (p_a[2] as i32 - p_b[2] as i32).abs();
-		}
-
-		let lr = LUMA_R / 255.0;
-		let lg = LUMA_G / 255.0;
-		let lb = LUMA_B / 255.0;
-		let diff_sum = diff_sum_r as f64 * lr + diff_sum_g as f64 * lg + diff_sum_b as f64 * lb;
-
- 		diff_sum / (num_pixels as f64 / skip_step as f64)
-	}
-
-	pub fn color_transform(image: &RgbImage, matrix: [f64; 12]) -> RgbImage {
-		let mut transformed_image = image.clone();
-		for (_x, _y, pixel) in transformed_image.enumerate_pixels_mut() {
-			let channels = pixel.channels();
-			let o_r = channels[0] as f64;
-			let o_g = channels[1] as f64;
-			let o_b = channels[2] as f64;
-			let n_r = ((o_r * matrix[0] + o_g * matrix[1] + o_b * matrix[ 2] + matrix[ 3]).round()).max(0.0).min(255.0) as u8;
-			let n_g = ((o_r * matrix[4] + o_g * matrix[5] + o_b * matrix[ 6] + matrix[ 7]).round()).max(0.0).min(255.0) as u8;
-			let n_b = ((o_r * matrix[8] + o_g * matrix[9] + o_b * matrix[10] + matrix[11]).round()).max(0.0).min(255.0) as u8;
-			*pixel = image::Rgb([n_r, n_g, n_b]);
-		}
-		transformed_image
 	}
 }
--- src/generator/utils/image.rs
+++ src/generator/utils/image.rs
@@ -1,3 +1,9 @@
+use image::{Pixel, RgbImage};
+
+const LUMA_R: f64 = 0.2126;
+const LUMA_G: f64 = 0.7152;
+const LUMA_B: f64 = 0.0722;
+
 #[inline(always)]
 pub fn blend_pixel(bottom: &[u8], top: &[u8], alpha: f64) -> [u8; 3] {
 	// Return early if no need to blend
@@ -13,4 +19,47 @@ pub fn blend_pixel(bottom: &[u8], top: &[u8], alpha: f64) -> [u8; 3] {
 	let ng: u8 = (top[1] as f64 * alpha + bottom[1] as f64 * alpha_n).round() as u8;
 	let nb: u8 = (top[2] as f64 * alpha + bottom[2] as f64 * alpha_n).round() as u8;
 	[nr, ng, nb]
+}
+
+pub fn diff(a: &RgbImage, b: &RgbImage) -> f64 {
+	let w = a.dimensions().0;
+	let h = a.dimensions().1;
+	let num_pixels = w * h;
+
+	let mut diff_sum_r: i32 = 0;
+	let mut diff_sum_g: i32 = 0;
+	let mut diff_sum_b: i32 = 0;
+
+	let samples_a = a.as_flat_samples().samples;
+	let samples_b = b.as_flat_samples().samples;
+
+	let skip_step = 1;
+
+	for (p_a, p_b) in samples_a.chunks_exact(3).zip(samples_b.chunks_exact(3)).step_by(skip_step) {
+		diff_sum_r += (p_a[0] as i32 - p_b[0] as i32).abs();
+		diff_sum_g += (p_a[1] as i32 - p_b[1] as i32).abs();
+		diff_sum_b += (p_a[2] as i32 - p_b[2] as i32).abs();
+	}
+
+	let lr = LUMA_R / 255.0;
+	let lg = LUMA_G / 255.0;
+	let lb = LUMA_B / 255.0;
+	let diff_sum = diff_sum_r as f64 * lr + diff_sum_g as f64 * lg + diff_sum_b as f64 * lb;
+
+	 diff_sum / (num_pixels as f64 / skip_step as f64)
+}
+
+pub fn color_transform(image: &RgbImage, matrix: [f64; 12]) -> RgbImage {
+	let mut transformed_image = image.clone();
+	for (_x, _y, pixel) in transformed_image.enumerate_pixels_mut() {
+		let channels = pixel.channels();
+		let o_r = channels[0] as f64;
+		let o_g = channels[1] as f64;
+		let o_b = channels[2] as f64;
+		let n_r = ((o_r * matrix[0] + o_g * matrix[1] + o_b * matrix[ 2] + matrix[ 3]).round()).max(0.0).min(255.0) as u8;
+		let n_g = ((o_r * matrix[4] + o_g * matrix[5] + o_b * matrix[ 6] + matrix[ 7]).round()).max(0.0).min(255.0) as u8;
+		let n_b = ((o_r * matrix[8] + o_g * matrix[9] + o_b * matrix[10] + matrix[11]).round()).max(0.0).min(255.0) as u8;
+		*pixel = image::Rgb([n_r, n_g, n_b]);
+	}
+	transformed_image
 }
