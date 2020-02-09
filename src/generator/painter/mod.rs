4c51d6da1dd0b47edf625d670b8f2ca46778f567
Zeh Fernando <zeh@work.co>
2020-02-09 17:18:12 -0500

    _use-enums

--- src/generator/painter/mod.rs
+++ src/generator/painter/mod.rs
@@ -3,7 +3,27 @@ use image::{RgbImage};
 pub mod rect;
 pub mod circle;
 
+pub enum PainterType {
+	Circle,
+	Rect,
+}
+
 pub trait Painter {
 	fn new() -> Self;
 	fn paint(&self, canvas: &RgbImage) -> RgbImage;
+}
+
+pub fn create_painter(painter_type: PainterType) -> impl Painter {
+	// if painter_type == PainterType::Rect {
+	// 	return rect::RectPainter::new();
+	// } else {
+	// 	return rect::RectPainter::new();
+	// }
+	let painter = match painter_type {
+		PainterType::Circle => circle::CirclePainter::new(),
+		PainterType::Rect => rect::RectPainter::new(),
+		_ => rect::RectPainter::new(),
+	};
+	painter
+	// p
 }
