59899c7830768956f1c282297441e2809e0fbc88
Zeh Fernando <zeh@work.co>
2020-02-14 09:46:48 -0500

    Utils: added functions to generate random numbers with bias

--- src/generator/utils/random.rs
+++ src/generator/utils/random.rs
@@ -9,6 +9,20 @@ pub fn get_random_range(rng: &mut rngs::ThreadRng, min: f64, max: f64) -> f64 {
 }
 
 #[inline(always)]
+pub fn get_random_range_bias(rng: &mut rngs::ThreadRng, min: f64, max: f64, bias: f64) -> f64 {
+	if min == max {
+		return min;
+	};
+	let mut r = rng.gen_range(0.0f64, 1.0f64);
+	if bias < 0.0f64 {
+		r = r.powf(-bias + 1.0f64);
+	} else if bias > 0.0f64 {
+		r = 1.0f64 - (1.0f64 - r).powf(bias + 1.0f64);
+	}
+	min + r * (max - min)
+}
+
+#[inline(always)]
 pub fn get_random_int(rng: &mut rngs::ThreadRng, min: u32, max: u32) -> u32 {
 	if min == max {
 		return min;
@@ -19,4 +33,9 @@ pub fn get_random_int(rng: &mut rngs::ThreadRng, min: u32, max: u32) -> u32 {
 pub fn get_random_ranges(rng: &mut rngs::ThreadRng, ranges: &Vec<(f64, f64)>) -> f64 {
 	let range: (f64, f64) = ranges[get_random_int(rng, 0, ranges.len() as u32) as usize];
 	get_random_range(rng, range.0, range.1)
+}
+
+pub fn get_random_ranges_bias(rng: &mut rngs::ThreadRng, ranges: &Vec<(f64, f64)>, bias: f64) -> f64 {
+	let range: (f64, f64) = ranges[get_random_int(rng, 0, ranges.len() as u32) as usize];
+	get_random_range_bias(rng, range.0, range.1, bias)
 }
