9eb4edbfff993800bc4fc29d7aad73791c741fba
Zeh Fernando <zeh@zehfernando.com>
2020-02-19 15:25:33 -0500

    Utils: added functions to generate noise

--- src/generator/utils/random.rs
+++ src/generator/utils/random.rs
@@ -1,4 +1,5 @@
 use rand::{Rng, rngs};
+use std::f64::consts::{PI};
 
 #[inline(always)]
 pub fn get_random_range(rng: &mut rngs::ThreadRng, min: f64, max: f64) -> f64 {
@@ -38,4 +39,31 @@ pub fn get_random_ranges(rng: &mut rngs::ThreadRng, ranges: &Vec<(f64, f64)>) ->
 pub fn get_random_ranges_bias(rng: &mut rngs::ThreadRng, ranges: &Vec<(f64, f64)>, bias: f64) -> f64 {
 	let range: (f64, f64) = ranges[get_random_int(rng, 0, ranges.len() as u32) as usize];
 	get_random_range_bias(rng, range.0, range.1, bias)
+}
+
+pub fn get_random_noise_sequence(rng: &mut rngs::ThreadRng, min: f64, max: f64) -> [f64; 256] {
+	let mut sequence = [0f64; 256];
+	for i in 0..256 {
+		sequence[i] = rng.gen_range(min, max);
+	}
+	return sequence;
+}
+
+#[inline(always)]
+pub fn get_noise_value(noise: [f64; 256], position: f64) -> f64 {
+	let pp = if position < 0.0 { 1.0 - position.abs() } else { position };
+	let pp = pp.fract() * 256.0f64;
+	let p1 = pp.floor() as usize;
+	let p2 = (p1 + 1) % 256;
+
+	let v1 = noise[p1];
+	let v2 = noise[p2];
+
+	// Phase
+	let f = pp.fract();
+
+	// Remap phase for smoothstep
+	let f = (1.0 - (f * PI).cos()) * 0.5;
+
+	v1 + (v2 - v1) * f
 }
