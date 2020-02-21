462901b25149641c384ccd57a1e11807a37b41c2
Zeh Fernando <zeh@work.co>
2020-02-21 17:27:03 -0500

    Utils: added pixel/fraction unit enums

--- src/generator/utils/mod.rs
+++ src/generator/utils/mod.rs
@@ -2,3 +2,4 @@ pub mod geom;
 pub mod image;
 pub mod parsing;
 pub mod random;
+pub mod units;
---
+++ src/generator/utils/units.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, PartialEq)]
+pub enum SizeUnit {
+	Fraction(f64),
+	Pixels(i32),
+}
+
+impl SizeUnit {
+	fn to_pixels(&self, total_size: i32) -> i32 {
+        match self {
+            Self::Fraction(value) => (*value * total_size as f64).round() as i32,
+            Self::Pixels(value) => *value,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_size_unit() {
+		// Fraction
+		assert_eq!(SizeUnit::Fraction(0.0).to_pixels(100), 0);
+		assert_eq!(SizeUnit::Fraction(0.5).to_pixels(100), 50);
+		assert_eq!(SizeUnit::Fraction(0.494).to_pixels(100), 49);
+		assert_eq!(SizeUnit::Fraction(0.178).to_pixels(100), 18);
+
+		// Pixels
+		assert_eq!(SizeUnit::Pixels(10).to_pixels(100), 10);
+		assert_eq!(SizeUnit::Pixels(20).to_pixels(200), 20);
+		assert_eq!(SizeUnit::Pixels(30).to_pixels(100), 30);
+		assert_eq!(SizeUnit::Pixels(40).to_pixels(200), 40);
+	}
+}
