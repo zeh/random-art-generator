4a079ea6ecf76ec199281ce48eb7c998bf51e73f
Zeh Fernando <zeh@zehfernando.com>
2020-11-27 15:21:51 -0500

    Utils: added a new "Margins" struct
    This can hold generic units (including a SizeUnit conversion method) for all sides.

--- src/generator/utils/units.rs
+++ src/generator/utils/units.rs
@@ -13,6 +13,25 @@ impl SizeUnit {
 	}
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct Margins<T> {
+	pub top: T,
+	pub right: T,
+	pub bottom: T,
+	pub left: T,
+}
+
+impl Margins<SizeUnit> {
+	pub fn to_pixels(&self, width: u32, height: u32) -> Margins<i64> {
+		Margins::<i64> {
+			top: self.top.to_pixels(height),
+			right: self.right.to_pixels(width),
+			bottom: self.bottom.to_pixels(height),
+			left: self.left.to_pixels(width),
+		}
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -30,5 +49,34 @@ mod tests {
 		assert_eq!(SizeUnit::Pixels(20).to_pixels(200), 20);
 		assert_eq!(SizeUnit::Pixels(30).to_pixels(100), 30);
 		assert_eq!(SizeUnit::Pixels(40).to_pixels(200), 40);
+	}
+
+	#[test]
+	fn test_margins() {
+		let ms = Margins::<SizeUnit> {
+			top: SizeUnit::Fraction(0.5),
+			right: SizeUnit::Fraction(0.123),
+			bottom: SizeUnit::Fraction(0.987),
+			left: SizeUnit::Fraction(0.0),
+		};
+
+		assert_eq!(
+			ms.to_pixels(200, 300),
+			Margins::<i64> {
+				top: 150,
+				right: 25,
+				bottom: 296,
+				left: 0,
+			}
+		);
+		assert_eq!(
+			ms.to_pixels(100, 100),
+			Margins::<i64> {
+				top: 50,
+				right: 12,
+				bottom: 99,
+				left: 0,
+			}
+		);
 	}
 }
