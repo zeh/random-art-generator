762e6270fbb99d0c05087ee258b8027b1d8eac83
Zeh Fernando <zeh@zehfernando.com>
2020-12-17 13:07:54 -0500

    Units: added a new Rectangle unit
    This can be used to declare spaces in images.

--- src/generator/utils/units.rs
+++ src/generator/utils/units.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, AddAssign, SubAssign};
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum SizeUnit {
 	Fraction(f64),
@@ -10,6 +12,26 @@ impl SizeUnit {
 			Self::Fraction(value) => (*value * total_size as f64).round() as i64,
 			Self::Pixels(value) => *value,
 		}
+	}
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Rectangle<T> {
+	pub x: T,
+	pub y: T,
+	pub width: T,
+	pub height: T,
+}
+
+impl<T> Rectangle<T>
+where
+	T: Copy + AddAssign<T> + Add<T, Output = T> + SubAssign<T>,
+{
+	pub fn apply_margins(&mut self, margins: Margins<T>) {
+		self.x += margins.left;
+		self.y += margins.top;
+		self.width -= margins.left + margins.right;
+		self.height -= margins.top + margins.bottom;
 	}
 }
 
@@ -49,6 +71,33 @@ mod tests {
 		assert_eq!(SizeUnit::Pixels(20).to_pixels(200), 20);
 		assert_eq!(SizeUnit::Pixels(30).to_pixels(100), 30);
 		assert_eq!(SizeUnit::Pixels(40).to_pixels(200), 40);
+	}
+
+	#[test]
+	fn test_rect() {
+		let mut rect = Rectangle::<i64> {
+			x: 0,
+			y: 0,
+			width: 200,
+			height: 100,
+		};
+
+		assert_eq!(rect.x, 0);
+		assert_eq!(rect.y, 0);
+		assert_eq!(rect.width, 200);
+		assert_eq!(rect.height, 100);
+
+		rect.apply_margins(Margins::<i64> {
+			top: 0,
+			right: 10,
+			bottom: 20,
+			left: 30,
+		});
+
+		assert_eq!(rect.x, 30);
+		assert_eq!(rect.y, 0);
+		assert_eq!(rect.width, 160);
+		assert_eq!(rect.height, 80);
 	}
 
 	#[test]
