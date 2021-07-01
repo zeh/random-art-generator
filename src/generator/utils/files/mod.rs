5a70fd47612d78e9df034757439711caf212fb42
Zeh Fernando <zeh@zehfernando.com>
2021-07-01 15:00:52 -0400

    Utils: file writer uses generator functions

--- src/generator/utils/files/mod.rs
+++ src/generator/utils/files/mod.rs
@@ -1,5 +1,7 @@
+use std::io::prelude::*;
 use std::{fs::File, path::Path};
 
+use bytes;
 use bytes::{BufMut, BytesMut};
 use image::{DynamicImage, ImageFormat, RgbImage};
 use img_parts::{
@@ -35,13 +37,25 @@ impl FileFormat {
 
 pub fn write_image(image_buffer: RgbImage, path: &Path) {
 	let image = DynamicImage::ImageRgb8(image_buffer);
-	let mut image_file = File::create(path).expect("creating output file");
 	let image_format = FileFormat::from_path(path).expect("parsing image format");
+	let mut output_file = File::create(path).expect("creating output file");
 	// We could also have used "image.save(output_path)"
-	image.write_to(&mut image_file, image_format.get_native_format()).unwrap();
+	image.write_to(&mut output_file, image_format.get_native_format()).unwrap();
 }
 
-pub fn write_image_with_metadata(image_buffer: RgbImage, path: &Path, comments: Vec<String>) {
+pub fn generate_image(image_buffer: RgbImage, path: &Path) -> bytes::Bytes {
+	let image = DynamicImage::ImageRgb8(image_buffer);
+	let image_format = FileFormat::from_path(path).expect("parsing image format");
+	let mut image_writer = BytesMut::new().writer();
+	image.write_to(&mut image_writer, image_format.get_native_format()).expect("writing image to Bytes");
+	bytes::Bytes::from(image_writer.into_inner().freeze().to_vec())
+}
+
+pub fn generate_image_with_metadata(
+	image_buffer: RgbImage,
+	path: &Path,
+	comments: Vec<String>,
+) -> bytes::Bytes {
 	let image = DynamicImage::ImageRgb8(image_buffer);
 	let image_format = FileFormat::from_path(path).expect("parsing image format");
 
@@ -50,10 +64,10 @@ pub fn write_image_with_metadata(image_buffer: RgbImage, path: &Path, comments: 
 	image.write_to(&mut image_writer, image_format.get_native_format()).expect("writing image to Bytes");
 	let image_bytes = Bytes::from(image_writer.into_inner().freeze().to_vec());
 
+	let mut image_and_meta_writer = BytesMut::new().writer();
+
 	// Additional metadata
 	let meta_software = format!("Random Art Generator v{}", crate_version!());
-
-	let output_file = File::create(path).expect("creating output file with metadata");
 
 	// Save differently based on file format
 	match image_format {
@@ -70,7 +84,7 @@ pub fn write_image_with_metadata(image_buffer: RgbImage, path: &Path, comments: 
 			png.chunks_mut().insert(chunks - 1, comments_chunk);
 			png.chunks_mut().insert(chunks - 1, software_chunk);
 
-			png.encoder().write_to(output_file).expect("writing encoded PNG file");
+			png.encoder().write_to(&mut image_and_meta_writer).expect("writing encoded PNG file");
 		}
 		FileFormat::JPEG => {
 			// Is JPEG, add segments
@@ -84,7 +98,15 @@ pub fn write_image_with_metadata(image_buffer: RgbImage, path: &Path, comments: 
 			let segments = jpeg.segments_mut().len();
 			jpeg.segments_mut().insert(segments - 1, comments_segment);
 
-			jpeg.encoder().write_to(output_file).expect("writing encoded JPEG file");
+			jpeg.encoder().write_to(&mut image_and_meta_writer).expect("writing encoded JPEG file");
 		}
 	}
+
+	bytes::Bytes::from(image_and_meta_writer.into_inner().freeze().to_vec())
+}
+
+pub fn write_image_with_metadata(image_buffer: RgbImage, path: &Path, comments: Vec<String>) {
+	let image_bytes = generate_image_with_metadata(image_buffer, path, comments);
+	let mut output_file = File::create(path).expect("creating output file with metadata");
+	output_file.write(&image_bytes[..]).expect("writing output file with metadata");
 }
